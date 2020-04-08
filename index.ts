3acf415c8cb9407f871423c1e83838c5e896fe33
taishikato <taishi.k0903@gmail.com>
2020-04-08 13:11:21 -0700

    set proper type

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-const uploadToStorage = async (refPath: string, imageUrl: string, firebase: any): Promise<string> => {
-  const storageRef = firebase.storage().ref()
+import firebase from 'firebase'
+
+const uploadToStorage = async (refPath: string, imageUrl: string, initializedFirebase: typeof firebase): Promise<string> => {
+  const storageRef = initializedFirebase.storage().ref()
   const uploadRef = storageRef.child(refPath)
   await uploadRef.putString(imageUrl, 'data_url')
   const url = await uploadRef.getDownloadURL()

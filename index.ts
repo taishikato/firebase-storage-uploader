a47725e59a87d1077d89dda5caa6c0864dc315e1
taishikato <taishi.k0903@gmail.com>
2020-04-07 19:15:01 -0700

    Change lib option

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-import firebase from 'firebase'
-
-const uploadToStorage = async (refPath: string, imageUrl: string, firebase: firebase.app.App): Promise<string> => {
+const uploadToStorage = async (refPath: string, imageUrl: string, firebase: any): Promise<string> => {
   const storageRef = firebase.storage().ref()
   const uploadRef = storageRef.child(refPath)
   await uploadRef.putString(imageUrl, 'data_url')
